ea6cd3d48deffe57d8ccd921f0f597aba48c30f5
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-24 22:40:25 +0545

    first commit

---
+++ script/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontWeight: {
+        b: "700",
+        sb: "600",
+        mb: "500",
+        r: "400",
+        l: "300",
+      },
+    },
+  },
+};
